9ee45fae0b57c486b4e0693e412b5be790ca7ae1
Thyago <salc.icomp.ufam.edu.br>
2023-11-13 20:56:21 -0400

    teste falha

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import express from 'express';
 const app = express();
 
 
-app.listen(40rodando na porta 4000');
+app.listen(4000, ()=>{
+    
+    
+    console.log('rodando na porta 4000');
 });
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,7 +1,7 @@
 import {sum} from '../src/sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 3)).toBe(3);
  
 });
 
